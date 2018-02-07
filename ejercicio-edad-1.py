5d2d1ee9f67f69edd93b7c6732b77e1a984ad497
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-02-07 19:45:34 +0100

    Update ejercicio-edad-1.py

--- ejercicio-edad-1.py
+++ ejercicio-edad-1.py
@@ -1,6 +1,6 @@
 #Josué Laguna Alonso
 #07/02/2018
-edad = raw_input ("Introduzca su edad: ")
-if edad>=15
-and edad<=17
+edad = input ("Introduzca su edad: ")
+if edad >= 15
+and edad <= 17
 print "puedes entrar en la sesion de tarde"
