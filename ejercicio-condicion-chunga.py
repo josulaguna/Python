bf4c117cba5e72537d18cc948d436d55639aa1e6
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-02-08 13:57:09 +0100

    Update ejercicio-condicion-chunga.py

--- ejercicio-condicion-chunga.py
+++ ejercicio-condicion-chunga.py
@@ -1,1 +1,8 @@
-
+#Josué Laguna Alonso
+#08/02/2018
+#coding: utf8
+numero = input ("Indica un numero: ")
+if ((numero % 2 == 0) or ((numero >= -10) and (numero <= 40)) or (numero < 0)):
+  print "numero valido"
+else:
+  print "numero invalido"
