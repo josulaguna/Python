2ae2f013dcdda26a943bd5200882f3cc2dfea445
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-02-09 12:58:43 +0100

    Update ejercicio-mayor_menor.py

--- ejercicio-mayor_menor.py
+++ ejercicio-mayor_menor.py
@@ -1,1 +1,20 @@
+A = input ("Introduce el valor A: ")
+B = input ("Introduce el valor B: ")
+C = input ("Introduce el valor C: ")
+
+if (A == B) and (A == C):
+	print "Hay 3 valores iguales"
+	if (A == B) and (A 
+
+
+
+
+if (A > B) and (A > C)
+	print "El valor A es el mas grande"
+    if (B < C) :
+			print ""
+if B > A > C:
+	print "El valor B es el mas grande y el valor C es el mas pequeño"
+    else:
+		print "El valor C es el mas grande y el valor B es el mas pequeño"
 
