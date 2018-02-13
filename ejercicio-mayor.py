1219f48f06ff601542a7f3f9fbb599bad6f21e46
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-02-13 12:21:21 +0100

    Update ejercicio-mayor.py

--- ejercicio-mayor.py
+++ ejercicio-mayor.py
@@ -1,8 +1,12 @@
 #Josué Laguna Alonso
-#12/02/2018
+#13/02/2018
 valor1 = input ("Escribe el primer valor: ")
 valor2 = input ("Escribe el segundo valor: ")
 if (valor1 > valor2):
   print "El primer valor es mas grande que el segundo"
-  else:
-    print "El segundo valor es mas grande que el primero"
+else:
+  if (valor2 > valor1)
+      print "El segundo valor es mas grande que el primero"
+  else
+      print "Los dos numeros son iguales"
+   
