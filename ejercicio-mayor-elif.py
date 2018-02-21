9afcda7265f89d1a228d7070e0fd7b959fffd0b4
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-02-21 22:19:55 +0100

    Update ejercicio-mayor-elif.py

--- ejercicio-mayor-elif.py
+++ ejercicio-mayor-elif.py
@@ -1,1 +1,10 @@
-
+#Josué Laguna Alonso
+#21/02/2018
+valor1 = input ("Escribe el primer valor: ")
+valor2 = input ("Escribe el segundo valor: ")
+if (valor1 > valor2):
+    print "El primer valor es mas grande que el segundo"
+elif (valor2 > valor1):
+    print "El segundo valor es mas grande que el primero"
+else:
+    print "Los dos numeros son iguales"
