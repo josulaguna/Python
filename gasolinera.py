97c8b00a5494d9c78dbb2a66d576dfe0115cc934
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-02-22 12:07:23 +0100

    Create gasolinera.py

---
+++ gasolinera.py
@@ -0,0 +1,18 @@
+#coding: utf8
+import os
+os.system ("clear")
+
+print """
+******************************************
+| Tipos: Super normal = 1                |
+|        Super turbo = 2                 |
+|        Sin plomo normal = 3            |
+|        Sin plomo con aditivos = 4      |
+|        Diesel normal = 5               |
+|        Diesel Fast&Furius = 6          |
+******************************************
+"""
+tipo_gasolina = input ("Que tipo de gasolina quiere? ")
+litros_gasolina = input ("Cuantos litros quiere? ")
+if tipo_gasolina = 1:
+	print 
