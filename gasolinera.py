312fb40445cde7eb8fb3e7b664d1f7aa6985321c
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-02-22 21:56:07 +0100

    Update gasolinera.py

--- gasolinera.py
+++ gasolinera.py
@@ -1,3 +1,5 @@
+#Josué Laguna Alonso
+#22/02/18
 #coding: utf8
 import os
 os.system ("clear")
@@ -16,6 +18,15 @@ print """
 tipo = input ("Que tipo de gasolina quiere? ")
 litros = input ("Cuantos litros quiere? ")
 
-if (tipo = 1):
-	print "Ha elegido la super normal, y ha elegido", litros "litros"
-
+if (tipo == 1):
+    print "Ha elegido la super normal, cantidad de litros:", litros ,"litros. Importe: ", litros * 1.5, "euros"
+if (tipo == 2):
+    print "Ha elegido la super turbo, cantidad de litros:", litros ,"litros. Importe: ", litros * 1.55, "euros"
+if (tipo == 3):
+    print "Ha elegido la sin plomo normal, cantidad de litros:", litros ,"litros. Importe: ", litros * 1.6, "euros"
+if (tipo == 4):
+    print "Ha elegido la sin plomo con aditivos, cantidad de litros:", litros ,"litros. Importe: ", litros * 1.65, "euros"
+if (tipo == 5):
+    print "Ha elegido la diesel normal, cantidad de litros:", litros ,"litros. Importe: ", litros * 1.7, "euros"
+if (tipo == 6):
+    print "Ha elegido la diesel Fast&Furius, cantidad de litros:", litros ,"litros. Importe: ", litros * 1.75, "euros"
