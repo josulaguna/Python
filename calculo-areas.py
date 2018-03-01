148cbb427c464f381aec8e3a3b7ab0f2a68e3399
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-03-01 17:10:12 +0100

    Update calculo-areas.py

--- calculo-areas.py
+++ calculo-areas.py
@@ -1,3 +1,5 @@
+#Josué Laguna Alonso
+#01/03/18
 #coding: utf8
 import os
 os.system ("clear")
@@ -18,13 +20,14 @@ figura = raw_input ("¿Qué figura quiere calcular (Escriba T o C)? ")
 if (figura == "T"):
     base = input ("Escriba la base: ")
     altura = input ("Escriba la altura: ")
-    if (base >=0 and altura >=0):
+    if (base >0 and altura >0):
         print "Un triangulo de base",base, "y de altura",altura, "tiene un área de ",base*altura/2
     else:
         print "No se puede calcular, ya que hay números negativos"
 
 if (figura == "C"):
     radio = input ("Escriba el radio: ")
-    if (radio >=0):
+    if (radio >0):
         print "El radio es",radio, "y tiene un área de " ,2*pi*radio
-
+    else:
+        print "No se puede calcular, ya que hay números negativos"
