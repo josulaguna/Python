5ee6b97d9b5b8841b55db6e74f16664321c10c3e
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-03-01 12:08:33 +0100

    Update calculo-areas.py

--- calculo-areas.py
+++ calculo-areas.py
@@ -1,1 +1,29 @@
+#coding: utf8
+import os
+os.system ("clear")
+from math import pi
+
+print """
+ ********************
+ Calculadora de áreas
+ ********************
+ 
+ a) Triángulo
+ b) Círculo
+ 
+"""
+ 
+figura = raw_input ("¿Qué figura quiere calcular (Escriba T o C)? ")
+
+if (figura == "T"):
+    base = input ("Escriba la base: ")
+    altura = input ("Escriba la altura: ")
+    if (base >=0 and altura >=0):
+        print "Un triangulo de base",base, "y de altura",altura, "tiene un área de"
+
+
+
+if (figura == "C"):
+    radio = input ("Escriba el radio: ")
+    if (radio >=0):
 
