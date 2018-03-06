36db758786d46213519ba430a4016e680f1ad043
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-03-06 03:06:04 +0100

    Update ejercicio-bucle-informes.py

--- ejercicio-bucle-informes.py
+++ ejercicio-bucle-informes.py
@@ -1,14 +1,14 @@
 #Josué Laguna Alonso
 #coding: utf8
-salir  = "N"
+salir = "N"
 numero = 2010 
 
-while ( salir=="N" ):
-	# Hago cosas
-	print "informe año:",numero
+while (salir=="N"):
+	#Hago cosas
+	print "informe año: ",numero
 
-	# Incremento
+	#Incremento
 	numero = numero +1
-	# Activo indicador de salida si toca
-	if ( numero >2016): # Condición de salida
+	#Activo indicador de salida si toca
+	if (numero >2016): #Condición de salida
     	    salir = "S"
