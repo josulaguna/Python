73413786817d41870fcabb586338c82bfc3b43b6
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-03-06 02:48:17 +0100

    Create ejercicio-bucle-contar-pares.py

---
+++ ejercicio-bucle-contar-pares.py
@@ -0,0 +1,16 @@
+#Josué Laguna Alonso
+#coding: utf8
+import time
+salir = "N"
+numero = 2
+numeromax2 = input ("Hasta que numero quieres que cuente?")
+while (salir=="N"):
+	#Hago cosas
+	print "informe año: ",numero
+    
+	#Incremento
+	numero = numero +2
+	time.sleep(0.1)
+	#Activo indicador de salida si toca
+	if (numero>numeromax2): # Condición de salida
+    	    salir = "S"
