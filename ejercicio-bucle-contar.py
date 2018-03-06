c84516ca4711abd29d12e18ab072151e2ac0011c
josulaguna <36137629+josulaguna@users.noreply.github.com>
2018-03-06 02:52:57 +0100

    Update ejercicio-bucle-contar.py

--- ejercicio-bucle-contar.py
+++ ejercicio-bucle-contar.py
@@ -3,14 +3,14 @@
 import time
 salir = "N"
 numero = 1 
-numeromax2  = input ("Hasta que numero quieres que cuente?")
+
 while (salir=="N"):
-	# Hago cosas
+	#Hago cosas
 	print "informe año: ",numero
     
-	# Incremento
+	#Incremento
 	numero = numero +1
 	time.sleep(0.1)
-	# Activo indicador de salida si toca
-	if (numero>numeromax2): # Condición de salida
-    	    salir = "S"    	    
+	#Activo indicador de salida si toca
+	if (numero >50): #Condición de salida
+    	    salir = "S"
